450482e8392efe7b2a755f06135ca98ebc1b0809
Daniel Li <danielli@v1040-wn-rt-c-54-82.campus-dynamic.uwaterloo.ca>
2020-01-09 15:43:36 -0500

    add face

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import face from './face.png';
 import resume from './Resume.pdf'
 import './App.css';
 
@@ -7,7 +7,7 @@ function Webpage() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={face} className="App-logo" alt="logo" />
         <p>
           Daniel Li (WIP)
         </p>
