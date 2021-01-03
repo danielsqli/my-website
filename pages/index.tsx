0fcddc4032a6c5db2328a6b383828d818ace02f9
idonlakyo <daniel.lisq12@gmail.com>
2021-01-02 21:23:03 -0500

    updated resume, removed deprecated links on website

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
 import Navigation from './Navigation'
 import Projects from './Projects'
 import Jumbotron from 'react-bootstrap/Jumbotron'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div style={ {} }>
+    <div>
+      <Head>
+        <title>Daniel Li</title>
+      </Head>
       <Navigation />
       <Jumbotron style={ {backgroundColor: "#384149"} } className="mb-0">
           <h1>Hello</h1>
           <p>
-            My Name is Daniel Li, a undergraduate Computer Science student at the Univeristy of Waterloo
+            My Name is Daniel Li, software developer and an undergraduate Computer Science student at the Univeristy of Waterloo
           </p>
       </Jumbotron>
       <Jumbotron style={ {backgroundColor: "#232627"} }>
